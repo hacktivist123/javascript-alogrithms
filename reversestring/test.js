b66812459fbf65d8dea3d3d10acd31ea97276b7c
hacktivist123 <akintayoshedrack@gmail.com>
2020-03-27 17:40:28 +0100

    reverse a string

---
+++ reversestring/test.js
@@ -0,0 +1,10 @@
+const reverse = require('./index');
+test('Reverse function exists', () => {
+  expect(reverse).toBeDefined();
+});
+test('Reverse reverses a string', () => {
+  expect(reverse('abcd')).toEqual('dcba');
+});
+test('Reverse reverses a string', () => {
+  expect(reverse('  abcd')).toEqual('dcba  ');
+});
